54dc98a194e25a475772cc44e1f20275f48918a8
Anton Kalmykov <anton.kalmykov@gmail.com>
2014-07-15 20:11:11 +0400

    Add QtSingleApplication library and change to single application mode

--- utility/main.cpp
+++ utility/main.cpp
@@ -1,19 +1,26 @@
-#include <QApplication>
 #include <QTextCodec>
 #include "forms/mainform.h"
+#include "qtsingleapplication/QtSingleApplication"
 
 
 int main(int argc, char** argv)
 {
-	QApplication application(argc, argv);
-
 	QTextCodec* codec = QTextCodec::codecForName("UTF-8");
 	QTextCodec::setCodecForCStrings(codec);
 	QTextCodec::setCodecForLocale(codec);
 	QTextCodec::setCodecForTr(codec);
 
+	QtSingleApplication application(argc, argv);
+	if(application.isRunning()) {
+		application.sendMessage(QString());
+		qDebug("Found another instance, exiting..");
+		return EXIT_SUCCESS;
+	}
+
 	MainForm mainForm;
 	mainForm.show();
+
+	application.setActivationWindow(&mainForm);
 
 	return application.exec();
 }
